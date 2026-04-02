d3c01b3fa875675b106c4b01d235caba41e6e438
weed <b.stromberg@data-systems.de>
2026-04-02 15:10:26 +0200

    Inital commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # simple-dyndns-uppdate
-A simple PowerShell script that handles your IPv6 und IPv6 DDNS (DynDNS) updates.
+A very simple PowerShell script that handles IPv4 und IPv6 DDNS (DynDNS) updates. Idea stolen from ugg.li. Must be installed on every, if v6 is used. This is not a 'centralized' solution.
+
+## Features
+- Custom Update URL, works everwhere
+- IPv4 and IPv6
+- Send updates only if IP/Prefix changed since last update
+
+## Installation
+
+
+## Usage
+Run PS from task Scheduler:
+`%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe`
+... with these arguments:
+`-noninteractive -file "PATH\simple-ddns-update.ps1" -ExecutionPolicy Bypass`
---
+++ simple-ddns-update.ps1
@@ -0,0 +1,41 @@
+# Supersimpler DDNS Client für PowerShell von ugg.li
+
+# --- configuration ---
+$updateURL = "https://kunden.aditsystems.de/liveconfig/hosting/dnsupdate"
+$hostname = "datenschleuder.gehirnbrand.de"
+$username = "datenschleuder"
+$password = "yorEcJuawpows-a33"
+$stateFile = "$env:ProgramData\simple-ddns-script_state.json"
+
+# --- IPs holen (v4 und v6)
+$ipv6 = (Resolve-DnsName myip.opendns.com -Server 2620:119:35::35 -Type AAAA).IPAddress
+$ipv4 = (Resolve-DnsName myip.opendns.com -Server 208.67.222.222 -Type A).IPAddress
+
+# --- IPv6 Präfix (z. B. /64) extrahieren, ggf. Prefix-Länge anpassen!
+$ipv6Prefix = ($ipv6 -split ":")[0..3] -join ":"
+
+# --- Alte Werte laden (falls vorhanden) ---
+$oldIPv4 = $null
+$oldIPv6Prefix = $null
+
+if (Test-Path $stateFile) {
+    $state = Get-Content $stateFile | ConvertFrom-Json
+    $oldIPv4 = $state.ipv4
+    $oldIPv6Prefix = $state.ipv6Prefix
+}
+
+# --- Aktuelle Werte mit alten vergleichen
+if ($ipv4 -ne $oldIPv4 -or $ipv6Prefix -ne $oldIPv6Prefix) {
+    Write-Host "Neue IP, Update wird gesendet ..."
+    curl.exe --user "${username}:${password}" "${updateURL}?hostname=${hostname}&myip=${ipv4}&myip6=${ipv6}"
+
+    # --- neue Werte speichern ---
+    $newState = @{
+        ipv4 = $ipv4
+        ipv6Prefix = $ipv6Prefix
+    }
+    $newState | ConvertTo-Json | Set-Content $stateFile
+} else {
+    Write-Host "Keine Aenderung der IP, kein Update."
+}
+
